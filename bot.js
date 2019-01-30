b0de241b19e853e9b7e99ac4734bcb4cc538a08f
MohamedMahmoud2019 <47193296+MohamedMahmoud2019@users.noreply.github.com>
2019-01-30 22:33:30 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("538460605523165204")
+setInterval(function() {
+channel.send(`The kings is here`);
+}, 30)
+})
+
+client.login(540267364164304897.c6l49poIJCEsVzspdPShdjlTdh8);
